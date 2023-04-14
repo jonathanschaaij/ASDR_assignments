f2685ba4113cae5d5fda245e8169cc48d8995401
Jonathan Schaaij <jonathan.schaaij@gmail.com>
2023-04-14 12:04:20 +0200

    asds

--- Assignment3/Code_Gen_3_2_2/README.md
+++ Assignment3/Code_Gen_3_2_2/README.md
@@ -8,9 +8,16 @@ This folder contains the generated code from Simulink and a method to test it ru
 # Step 1: Source the ROS executables
 source install/local_setup.bash
 
+# Also source the ros2-xenomai nodes
+. /home/pi/ros2-xenomai/install/local_setup.sh
+
 # Step 2: Compile the binaries
 colcon build
+# Maybe build the xenomai files seperately
+
 
 # Step 3: Launch the ROS nodes using the launchfile
 ros2 launch src/multisimlaunch.py
+
+# launch the xenomai executable in a new terminal.
 ```
--- Assignment3/Code_Gen_3_2_2/src/multisimlaunch.py
+++ Assignment3/Code_Gen_3_2_2/src/multisimlaunch.py
@@ -4,39 +4,50 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package="image_tools",
-            executable="cam2image",
-            name='camera',
-            parameters=[{"depth": 1}, {"history": "keep_last"}],
+            package="jiwy-simulator",
+            executable="jiwy-tester",
+            name='Manual_Setpoint',
         ),
         Node(
-            package='brightness_pkg',
-            executable='lightposition_node',
-            name='lightpositionnode',
-            arguments=[{"inputimage": "image"}] 
+            package="ros2-xenomai",
+            executable="talker",
+            name='Setpoint_to_XDDP',
+            parameters=[{"topicName": "sendData"}, {"xddpPort": 10}],
         ),
-        Node(
-            package='closed_loop',
-            executable='controller',
-            name='losed_loop_controller',
-        ),
+        # Node(
+        #     package="image_tools",
+        #     executable="cam2image",
+        #     name='camera',
+        #     parameters=[{"depth": 1}, {"history": "keep_last"}],
+        # ),
+        # Node(
+        #     package='brightness_pkg',
+        #     executable='lightposition_node',
+        #     name='lightpositionnode',
+        #     arguments=[{"inputimage": "image"}] 
+        # ),
+        # Node(
+        #     package='closed_loop',
+        #     executable='controller',
+        #     name='losed_loop_controller',
+        # ),
 
-        # Visualise the outputs
-        Node(
-            package='image_tools',
-            executable='showimage',
-            name='originalImage'
-        ),
-        Node(
-            package='image_tools',
-            executable='showimage',
-            name='grayImage',
-            arguments=["image:=/grayImage"]
-        ),
-        Node(
-            package='image_tools',
-            executable='showimage',
-            name='binaryImage',
-            arguments=["image:=/binaryImage"]
-        ),
+        # # Visualise the outputs
+        # Node(
+        #     package='image_tools',
+        #     executable='showimage',
+        #     name='originalImage'
+        # ),
+        # Node(
+        #     package='image_tools',
+        #     executable='showimage',
+        #     name='grayImage',
+        #     arguments=["image:=/grayImage"]
+        # ),
+        # Node(
+        #     package='image_tools',
+        #     executable='showimage',
+        #     name='binaryImage',
+        #     arguments=["image:=/binaryImage"]
+        # ),
     ])
